5b9d1b063842d72bf18af5afd944dc917b02fc1c
prayagraj2000 <prayagraj2000@gmail.com>
2021-07-29 21:43:27 +0530

    Harmonic Number Program

---
+++ harmonicnumber.java
@@ -0,0 +1,13 @@
+public class harmonicnumber
+{
+	public static void main(String arg[])
+	{
+	double n=10,i;
+	double sum=0;	
+	for( i=1;i<=n;i++)
+	{
+		sum=sum+(1/i);
+	}
+	System.out.println("Harmonic value = "+sum);
+	}
+}
